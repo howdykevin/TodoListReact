084a47677174502bf04d7f22c0c3182af2a92507
kevin <k.drago57@gmail.com>
2019-02-19 11:23:10 +1000

    TodoCard development
    Part 1

---
+++ src/components/Card.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
+
+class Todos extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Card>
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default Todos;
